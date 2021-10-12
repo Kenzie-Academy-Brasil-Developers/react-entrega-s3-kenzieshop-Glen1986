0e73dcb19fcdd58060407af72b5cbca8f1e64b9b
glen1986 <escobar.glen@gmail.com>
2021-10-12 20:11:44 -0300

    carroTotal

--- src/components/cart/styles.js
+++ src/components/cart/styles.js
@@ -1,17 +1,38 @@
 import styled from 'styled-components';
 export const Container = styled.div`
 display:flex;
+min-height:40rem;
 flex-direction:column;
 flex-wrap:wrap;
 top:-10rem;
-padding:2rem;
+padding:0 2rem;
 justify-content:center;
 border:2px solid #99ebff;
 background-color:white;
-margin: 0 6rem 0 6rem;
+margin: 0 6rem;
 .products{
   position:relative;
   top:-20rem;
-  margin-top:0;
+  margin-top:-4.25rem;;
+}
+  .products>div>h3, .products>div>div{
+    margin-right:2rem;
+  }
+  .products>div{
+    display:flex;
+    width:40rem
+  }
+  .products>div, .products>div>img{
+    display:flex;
+
+    text-align:center;
+  }
+  .products>div> img{
+    display:none;
+  }
+  .products>div>button{
+    margin: 1rem;
+  }
+
 }
 `;
--- src/components/total/index.js
+++ src/components/total/index.js
@@ -1,11 +1,23 @@
 import {Container} from "./styles"
+import {useSelector} from 'react-redux';
 const Total = () => {
+  const product = useSelector((store) => store.cart);
+  const total = product.map(item => item.price).reduce((a, b) => (a + b))
   return (
     <Container>
       <h3>Total da suas Compras</h3>
-      <button>Pagar Total</button>
+      {product.map((item, index) => (
+        <tr key={index} >
+          <td className="name"> {item.name} </td>
+          <td className="price"> {item.price} </td>
+        </tr>
 
-    </Container>
+      ))
+      }
+      <tr><td>total a pagar </td><td>{product.map(item => item.price).reduce((a, b) => (a + b))}</td></tr>
+      <button onClick={() => alert(`voce realizou uma compra por: ${total}`)}>Pagar Total</button>
+
+    </Container >
   )
 }
 export default Total;
