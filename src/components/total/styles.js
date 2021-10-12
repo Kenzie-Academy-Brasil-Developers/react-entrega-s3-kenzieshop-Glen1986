25dc71bd33f4b284616a2b3b6e2980d2d4b47d57
glen1986 <escobar.glen@gmail.com>
2021-10-12 15:22:07 -0300

    carroVisual

---
+++ src/components/total/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+export const Container = styled.div`
+border: 2px solid black; 
+height:20rem;
+width:20rem;
+margin-left:auto;
+align-items:center;
+  >button{
+  height:4rem;
+  margin-bottom:2rem;
+  font-size:1rem;
+  background-color: #99ebff;
+  cursor:pointer;
+  border-radius:15px;
+  padding: 0 3rem;
+}
+`;
